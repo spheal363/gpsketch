06dc88ddca6ad3bfb7462fb16e9cb5b6f1bbc904
spheal363 <115055083+spheal363@users.noreply.github.com>
2025-04-26 14:43:40 +0900

    fix: POINT型を緯度経度で分割

--- seed.sql
+++ seed.sql
@@ -1,10 +1,10 @@
 USE touka_db;
 
 -- routes テーブルに仮データを挿入
-INSERT INTO routes (id, animal_name, distance_km, actual_route_distance_km, route_geojson, stat_point, image_url, image_bounds)
+INSERT INTO routes (id, animal_name, distance_km, actual_route_distance_km, route_geojson, stat_end_latitude, stat_end_longitude, image_url, image_bounds)
 VALUES
-  (UUID(), 'ねこ', 5.0, 5.2, '{"type": "LineString", "coordinates": [[139.6917, 35.6895], [139.7000, 35.6900]]}', ST_GeomFromText('POINT(139.6917 35.6895)'), 'https://example.com/cat.png', '{"northwest": [139.6900, 35.6900], "southeast": [139.7000, 35.6800]}'),
-  (UUID(), 'いぬ', 3.0, 3.1, '{"type": "LineString", "coordinates": [[139.7000, 35.6800], [139.7100, 35.6850]]}', ST_GeomFromText('POINT(139.7000 35.6800)'), 'https://example.com/dog.png', '{"northwest": [139.6950, 35.6850], "southeast": [139.7100, 35.6750]}');
+  (UUID(), 'ねこ', 5.0, 5.2, '{"type": "LineString", "coordinates": [[139.6917, 35.6895], [139.7000, 35.6900]]}', 35.6895, 139.6917, 'https://example.com/cat.png', '{"northwest": [139.6900, 35.6900], "southeast": [139.7000, 35.6800]}'),
+  (UUID(), 'いぬ', 3.0, 3.1, '{"type": "LineString", "coordinates": [[139.7000, 35.6800], [139.7100, 35.6850]]}', 35.6800, 139.7000, 'https://example.com/dog.png', '{"northwest": [139.6950, 35.6850], "southeast": [139.7100, 35.6750]}');
 
 -- runs テーブルに仮データを挿入
 INSERT INTO runs (id, route_id, start_time, end_time, actual_distance_km, pace_min_per_km, calories, track_geojson)
